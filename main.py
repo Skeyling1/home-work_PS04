15d9ee17660bf580ce33b6b4dc37c40d6f52bb7d
Skeyling1 <skeyling1@gmail.com>
2024-07-12 20:51:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,16 @@ while True:
             input()
 
     elif user_choise == "2":
-        browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+        new_ref = input("введите ключевое слово")
+        for element in browser.find_elements(By.TAG_NAME, "a"):
+            title = element.get_attribute("title")
+            if title == new_ref:
+                link = element.get_attribute("href")
+
+
+        browser.get(link)
+
+
 
 
 

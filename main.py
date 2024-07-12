bef34c6f7f6f25c34799d33003ded41fa006be7a
Skeyling1 <skeyling1@gmail.com>
2024-07-12 19:03:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,21 +19,33 @@ user_req = input("введите запрос")
 
 browser = webdriver.Firefox()
 browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
-
 assert "Википедия" in browser.title
-
 
 #Находим окно поиска
 search_box = browser.find_element(By.ID, "searchInput")
 #Прописываем ввод текста в поисковую строку. В кавычках тот текст, который нужно ввести
 search_box.send_keys(user_req)
-time.sleep(3)
+time.sleep(5)
 #Добавляем не только введение текста, но и его отправку
 search_box.send_keys(Keys.RETURN)
-time.sleep(5)
+
+while True:
+    print("""
+    Выберите действие:
+    1 - листать параграфы текущей статьи
+    2 - перейти на одну из связанных страниц
+    0 - выйти из программы
+    """)
+    user_choise = input()
+    if  user_choise == "0":
+        break
+    elif user_choise == "1":
+        pass
+    elif user_choise == "2":
+        pass
 
 
-time.sleep(30)
+
 
 
 

9ffc2aba32c840b8feb902c1748339129d071b1d
Skeyling1 <skeyling1@gmail.com>
2024-07-12 19:43:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,12 +40,14 @@ while True:
     if  user_choise == "0":
         break
     elif user_choise == "1":
-        pass
+        paragraphs = browser.find_elements(By.TAG_NAME, "p")
+        # Для перебора пишем цикл
+        for paragraph in paragraphs:
+            print(paragraph.text)
+            input()
+
     elif user_choise == "2":
-        pass
-
-
-
+        browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
 
 
 
